0e8ad5d2ab4223429d0c8c958a0789e92360682a
isaacokubi <izobrack3@gmail.com>
2025-12-12 23:47:32 +0300

    latest changes on HeaderTop.tsx

--- components/HeaderTop.tsx
+++ components/HeaderTop.tsx
@@ -22,7 +22,7 @@ const HeaderTop = () => {
           </div>
 
     <div className="text-gray-500 text-[12px]">
-      <b>NEW SCHOOL OF CHOICE</b> THIS WEEK ORDER OVER $55
+      <b>welcome all to our shop</b> THIS WEEK ORDER OVER $55
     </div>
 
     <div  className="flex gap-4">
